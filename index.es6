704018527bc1ba43a449677779c3a2600d27c6b1
Sam Lau <samlau95@gmail.com>
2018-01-06 00:52:51 -0800

    Transpile es6 to es5 :(

---
+++ index.es6
@@ -0,0 +1,69 @@
+// Log node version to help
+console.log(process.version)
+
+const fs = require('fs')
+
+const readSummary = filepath =>
+  fs
+    .readFileSync(filepath, { encoding: 'utf8' })
+    .trim()
+    .split('\n')
+const writeSummary = (filepath, lines) =>
+  fs.writeFileSync(filepath, str.join('\n'))
+
+const isLink = line => /^\s*(\*|-|\+)/.test(line)
+
+const isSubpart = (line, indent) => line.startsWith(indent)
+const isSubsubpart = (line, indent) => line.startsWith(`${indent}${indent}`)
+
+// To my knowledge, Gitbook disallows nesting more than 2 levels deep which is
+// why the section is a hard-coded 3-tuple of [part, subpart, subsubpart].
+function addSectionNumber(lines, section = [0, 0, 0], indent = '  ') {
+  if (lines.length === 0) {
+    return lines
+  }
+
+  const [line, ...rest] = lines
+  const [part, subpart, subsubpart] = section
+
+  if (!isLink(line)) {
+    return [line, ...addSectionNumber(rest, section, indent)]
+  }
+
+  const [before, after] = line.split('[')
+
+  // We only increment the numbering for a section *after* the recursion since
+  // we can't tell ahead of time whether the next link is nested.
+  if (isSubsubpart(line, indent)) {
+    const newLine = `${before}[${part}.${subpart}.${subsubpart + 1} ${after}`
+    return [
+      newLine,
+      ...addSectionNumber(rest, [part, subpart, subsubpart + 1], indent),
+    ]
+  } else if (isSubpart(line, indent)) {
+    const newLine = `${before}[${part}.${subpart + 1} ${after}`
+    return [newLine, ...addSectionNumber(rest, [part, subpart + 1, 0], indent)]
+  } else {
+    const newLine = `${before}[${part + 1}. ${after}`
+    return [newLine, ...addSectionNumber(rest, [part + 1, 0, 0], indent)]
+  }
+}
+
+module.exports = {
+  hooks: {
+    init: () => {
+      const root = this.resolve('')
+      const summaryFilename = this.config.get('structure.summary')
+      const filepath = `${root}/${summaryFilename}`
+
+      const lines = readSummary(filepath)
+      const linesWithSection = addSectionNumber(lines)
+      writeSummary(filepath, linesWithSection)
+    },
+  },
+
+  // Add functions to the exports for easier testing
+  testing: {
+    addSectionNumber,
+  },
+}
--- index.js
+++ index.js
@@ -1,75 +1,94 @@
-// Log node version to help debug
-console.log(process.version)
+'use strict';
 
-const fs = require('fs')
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-const readSummary = filepath =>
-  fs
-    .readFileSync(filepath, { encoding: 'utf8' })
-    .trim()
-    .split('\n')
-const writeSummary = (filepath, lines) =>
-  fs.writeFileSync(filepath, str.join('\n'))
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-const isLink = line => /^\s*(\*|-|\+)/.test(line)
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
 
-const isSubpart = (line, indent) => line.startsWith(indent)
-const isSubsubpart = (line, indent) => line.startsWith(`${indent}${indent}`)
+// Log node version to help
+console.log(process.version);
+
+var fs = require('fs');
+
+var readSummary = function readSummary(filepath) {
+  return fs.readFileSync(filepath, { encoding: 'utf8' }).trim().split('\n');
+};
+var writeSummary = function writeSummary(filepath, lines) {
+  return fs.writeFileSync(filepath, str.join('\n'));
+};
+
+var isLink = function isLink(line) {
+  return (/^\s*(\*|-|\+)/.test(line)
+  );
+};
+
+var isSubpart = function isSubpart(line, indent) {
+  return line.startsWith(indent);
+};
+var isSubsubpart = function isSubsubpart(line, indent) {
+  return line.startsWith('' + indent + indent);
+};
 
 // To my knowledge, Gitbook disallows nesting more than 2 levels deep which is
 // why the section is a hard-coded 3-tuple of [part, subpart, subsubpart].
-function addSectionNumber(lines, section, indent) {
+function addSectionNumber(lines) {
+  var section = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [0, 0, 0];
+  var indent = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '  ';
+
   if (lines.length === 0) {
-    return lines
+    return lines;
   }
 
-  const line = lines[0]
-  const rest = lines.slice(1)
+  var _lines = _toArray(lines),
+      line = _lines[0],
+      rest = _lines.slice(1);
 
-  const part = section[0]
-  const subpart = section[1]
-  const subsubpart = section[2]
+  var _section = _slicedToArray(section, 3),
+      part = _section[0],
+      subpart = _section[1],
+      subsubpart = _section[2];
 
   if (!isLink(line)) {
-    return [line].concat(addSectionNumber(rest, section, indent))
+    return [line].concat(_toConsumableArray(addSectionNumber(rest, section, indent)));
   }
 
-  const before = line.split('[')[0]
-  const after = line.split('[')[1]
+  var _line$split = line.split('['),
+      _line$split2 = _slicedToArray(_line$split, 2),
+      before = _line$split2[0],
+      after = _line$split2[1];
 
   // We only increment the numbering for a section *after* the recursion since
   // we can't tell ahead of time whether the next link is nested.
+
+
   if (isSubsubpart(line, indent)) {
-    const newLine = `${before}[${part}.${subpart}.${subsubpart + 1} ${after}`
-    return [newLine].concat(
-      addSectionNumber(rest, [part, subpart, subsubpart + 1], indent),
-    )
+    var newLine = before + '[' + part + '.' + subpart + '.' + (subsubpart + 1) + ' ' + after;
+    return [newLine].concat(_toConsumableArray(addSectionNumber(rest, [part, subpart, subsubpart + 1], indent)));
   } else if (isSubpart(line, indent)) {
-    const newLine = `${before}[${part}.${subpart + 1} ${after}`
-    return [newLine].concat(
-      addSectionNumber(rest, [part, subpart + 1, 0], indent),
-    )
+    var _newLine = before + '[' + part + '.' + (subpart + 1) + ' ' + after;
+    return [_newLine].concat(_toConsumableArray(addSectionNumber(rest, [part, subpart + 1, 0], indent)));
   } else {
-    const newLine = `${before}[${part + 1}. ${after}`
-    return [newLine].concat(addSectionNumber(rest, [part + 1, 0, 0], indent))
+    var _newLine2 = before + '[' + (part + 1) + '. ' + after;
+    return [_newLine2].concat(_toConsumableArray(addSectionNumber(rest, [part + 1, 0, 0], indent)));
   }
 }
 
 module.exports = {
   hooks: {
-    init: () => {
-      const root = this.resolve('')
-      const summaryFilename = this.config.get('structure.summary')
-      const filepath = `${root}/${summaryFilename}`
+    init: function init() {
+      var root = undefined.resolve('');
+      var summaryFilename = undefined.config.get('structure.summary');
+      var filepath = root + '/' + summaryFilename;
 
-      const lines = readSummary(filepath)
-      const linesWithSection = addSectionNumber(lines, [0, 0, 0], '  ')
-      writeSummary(filepath, linesWithSection)
-    },
+      var lines = readSummary(filepath);
+      var linesWithSection = addSectionNumber(lines);
+      writeSummary(filepath, linesWithSection);
+    }
   },
 
   // Add functions to the exports for easier testing
   testing: {
-    addSectionNumber,
-  },
-}
+    addSectionNumber: addSectionNumber
+  }
+};
